98e1bac5f2720863cc118d16ce8f8f8116ac05f9
Luitdesign <info@luitdesign.nl>
2026-01-17 13:53:19 +0100

    Delete public directory

--- public/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/../app/bootstrap.php';
-
-if (!is_logged_in()) {
-    redirect('/login.php');
-}
-redirect('/months.php');
--- public/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/../app/bootstrap.php';
-
-auth_logout();
-redirect('/login.php');
