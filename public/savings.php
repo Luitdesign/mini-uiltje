17c7d869873f9b4974c2e5ebab6cffbcc053fe53
Luitdesign <info@luitdesign.nl>
2026-02-20 15:41:53 +0100

    Update savings.php

--- public/savings.php
+++ public/savings.php
@@ -154,7 +154,7 @@ render_header('Savings', 'savings');
           <th>Current balance</th>
           <th>Default monthly amount</th>
           <th>Top-up category</th>
-          <th style="width:260px">Actions</th>
+          <th style="width:280px">Actions</th>
         </tr>
       </thead>
       <tbody>
